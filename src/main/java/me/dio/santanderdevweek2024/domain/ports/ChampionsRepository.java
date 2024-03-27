b45ce7ab8741991dee55b73f91e574bcee9db1c3
vrfjr <vagnerrfjr@gmail.com>
2024-03-27 20:49:43 -0300

    commit 0.1

---
+++ src/main/java/me/dio/santanderdevweek2024/domain/ports/ChampionsRepository.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2024.domain.ports;
+
+import me.dio.santanderdevweek2024.domain.model.Champions;
+import java.util.List;
+import java.util.Optional;
+
+
+
+public interface ChampionsRepository {
+    List<Champions> findAll();
+
+    Optional<Champions> findOne(Long id);
+}
